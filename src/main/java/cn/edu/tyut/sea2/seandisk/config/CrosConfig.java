b278ff12aaf718f1a4475595932f577475194abe
王子龙 <wzl709@outlook.com>
2022-01-11 20:01:53 +0800

    fix 因为不知名原因无法使用AOP，暂时关闭

--- src/main/java/cn/edu/tyut/sea2/seandisk/config/CrosConfig.java
+++ src/main/java/cn/edu/tyut/sea2/seandisk/config/CrosConfig.java
@@ -14,7 +14,7 @@ public class CrosConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-//                .allowedOriginPatterns("*")
+                .allowedOriginPatterns("*")
                 .allowCredentials(true)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .maxAge(3600);
